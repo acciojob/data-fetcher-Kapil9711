5f7bd2db83595e3b95105e1215c6735ac3519556
kapil <kapil.personal9711@gmail.com>
2024-06-24 23:01:11 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,27 @@ import "./../styles/App.css";
 const api = " https://dummyjson.com/products";
 
 const App = () => {
-  const [data, useData] = useState("");
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
   useEffect(() => {
+    setIsError(false);
+    setIsLoading(true);
     fetch(api)
       .then((res) => res.json())
-      .then((d) => useData(d))
-      .catch((e) => e);
+      .then((d) => {
+        setData(d);
+        setIsLoading(false);
+      })
+      .catch((e) => setIsError(e.message));
   }, []);
 
   return (
     <div>
-      <h1>
-        <pre>{data && JSON.stringify(data)}</pre>
-      </h1>
+      <h1>Data Fetched from API</h1>
+      {isLoading ? <p>Loading...</p> : <p>An error occurred: {isError}</p>}
+      <pre>{data && JSON.stringify(data)}</pre>
     </div>
   );
 };
