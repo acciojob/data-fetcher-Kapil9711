bd1689532ce7e4a4e37c9eee63f1427d000dc684
kapil <kapil.personal9711@gmail.com>
2024-06-24 22:49:58 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+const api = " https://dummyjson.com/products";
 
 const App = () => {
+  const [data, useData] = useState("");
+  useEffect(() => {
+    fetch(api)
+      .then((res) => res.json())
+      .then((d) => useData(d));
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <pre>{data && JSON.stringify(data)}</pre>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
